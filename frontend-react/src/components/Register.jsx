f4509641ad58700fffc4ca3ceed2f34b2ca54866
Govindaspkta <sapgovinda05@gmail.com>
2025-08-27 13:44:40 +0545

    registration validated along wiht succesful registration

--- frontend-react/src/components/Register.jsx
+++ frontend-react/src/components/Register.jsx
@@ -6,6 +6,9 @@ const Register = () => {
   const [username,setUsername]=useState('')
   const [email,setEmail]=useState('')
   const [password,setpassword]=useState('')
+  const [errors,setErrors]=useState({})
+  const [success,setSuccess]=useState(false)
+
   const handleRegistration = async (e) =>{
     e.preventDefault();
     const userData={
@@ -15,8 +18,11 @@ try{
   const response= await axios.post('http://127.0.0.1:8000/api/v1/register/',userData)
   console.log('response.data ==>',response.data)
   console.log("resgistrtion succesful")
+  setSuccess(true)
+  setErrors('')
 }  
 catch(error){
+  setErrors(error.response.data)
   console.log('Registration error:',error.response.data)
 
 
@@ -34,9 +40,17 @@ catch(error){
     <h3 className="text-light text-center mb-5">Create an Account</h3>
     <form onSubmit={handleRegistration}>
       <input type="text" className="form-control mb-3" placeholder="username please" value={username} onChange={(e)=> setUsername(e.target.value)} />
+      <small>{errors.username && <div className="text-danger"> {errors.username}</div>}</small>
+
+      <div className="mb-3">
       <input type="email" className="form-control mb-3" placeholder="email Please"  value={email} onChange={(e)=>setEmail(e.target.value)} />
+     </div>
+
+      <div className="mb-2">
         <input type="password" className="form-control mb-5" placeholder="password please" value={password} onChange={(e)=>setpassword(e.target.value)} />
-    
+           <small>{errors.password && <div className="text-danger"> {errors.password}</div>}</small>
+      </div>
+      {success && <div className="alert alert-success">Registration Successful</div>}
         <button type="submit" className="btn btn-info d-block mx-auto" >Register
         </button>
 
