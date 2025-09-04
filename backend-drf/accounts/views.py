b7770a71c968703852634af1ad2a06a0180181ec
Govindaspkta <sapgovinda05@gmail.com>
2025-09-05 00:52:42 +0545

    working on auth

--- backend-drf/accounts/views.py
+++ backend-drf/accounts/views.py
@@ -3,12 +3,21 @@ from .serializers import UserSerializer
 from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework.permissions import AllowAny
-from rest_framework.views improt APIView
-
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 class RegisterView(generics.CreateAPIView):
     queryset=User.objects.all()
     serializer_class=UserSerializer
     permission_classes=[AllowAny]
 
 
-class ProtectedView()
+class ProtectedView(APIView):
+    permission_classes=[IsAuthenticated]
+    def self(self,request):
+        response={
+        'status':'Request was permitted'
+        }
+        
+        return Response(response)
+        
