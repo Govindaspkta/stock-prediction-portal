d27f335ba29dafbb2b92fb9eedcc230f0379b716
Govindaspkta <sapgovinda05@gmail.com>
2025-09-11 13:33:18 +0545

    added axios instance

--- frontend-react/src/components/dashboard/Dashboard.jsx
+++ frontend-react/src/components/dashboard/Dashboard.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import axios from 'axios'
+import axiosInstance from '../../axiosInstance'
 
 const Dashboard = () => {
-  return (
+  
+const accessToken=localStorage.getItem('access_token')     
+useEffect(() =>{
+    const fetchProtectedData = async() =>{
+        try{
+const response=await axiosInstance.get('/protected-view/',{
+    headers:{
+        Authorization: `Bearer ${accessToken}`
+    }
 
-    <>
-    
+})
+console.log("success: ",response.data);
+        }
+        catch(error ) {
+            console.error('error ins fetching data')
+        }
+    }
+    fetchProtectedData();
+ }, [])
+
+    return (
+        <>
     <div>Dashboard</div>
     </>
+
     
   )
 }
