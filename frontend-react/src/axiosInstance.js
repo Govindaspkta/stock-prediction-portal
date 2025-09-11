3be88d3a1f970e629ae152cdb2da019b025beb50
Govindaspkta <sapgovinda05@gmail.com>
2025-09-11 15:33:18 +0545

    added response interceptor

--- frontend-react/src/axiosInstance.js
+++ frontend-react/src/axiosInstance.js
@@ -24,4 +24,32 @@ axiosInstance.interceptors.request.use(
         return Promise.reject(error)
     }
 )
+//Rsponse interceptor
+axiosInstance.interceptors.response.use(
+    function(response){
+        return response
+    },
+    //handle failed responses
+   async function(error){
+        const originalRequest =error.config
+        if(error.response.status === 401 &&  !originalRequest.retry){
+            originalRequest.retry = true;
+            const refreshToken=localStorage.getItem('refreshToken')
+            try{
+                const response = await axiosInstance.post('/token/refresh/', {refresh:refreshToken})
+                console.log('new accesss token',response.data.access)
+localStorage.setItem('accessToken',response.data.access)
+originalRequest.headers['Authorization']  = `Bearer ${response.data.access}`
+return axiosInstance(originalRequest)          }
+            catch(error){
+              localStorage.removeItem('accessToken')
+            localStorage.removeItem('refreshToken')
+            window.location.href= '/login'
+
+            }
+          }
+          return Promise.reject(error);
+    }
+)
+
 export default axiosInstance
