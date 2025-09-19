95466e4d8d69163a86f85294cbe834a8d28bb0d5
Govindaspkta <sapgovinda05@gmail.com>
2025-09-19 15:27:43 +0545

    working on tickerd urls,views...

--- frontend-react/src/components/dashboard/Dashboard.jsx
+++ frontend-react/src/components/dashboard/Dashboard.jsx
@@ -1,9 +1,10 @@
-import React, {useEffect} from 'react'
+import React, {useEffect,useState} from 'react'
 import axios from 'axios'
 import axiosInstance from '../../axiosInstance'
 
 const Dashboard = () => {
-  
+  const [ticker,setTicker]=useState('')
+
 useEffect(() =>{
     const fetchProtectedData = async() =>{
         try{
@@ -21,7 +22,18 @@ console.log("success: ",response.data);
 
     return (
         <>
-    <div>Dashboard</div>
+ <div className="container">
+    <div className="row">
+        <div className="col-md-6 mx-auto">
+            <form  >
+                <input type="text" className='form-control' placeholder='Enter a Stock Ticker'
+                onChange={(e) => setTicker(e.target.value)}  required >
+                </input>
+            <button type='submit' className='btn bt-info-mt-3' >See Prediction</button>
+            </form>
+        </div>
+    </div>
+ </div>
     </>
 
     
